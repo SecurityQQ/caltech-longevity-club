44fba0d3ba1bac6d6cb547c45790f10d1b4126d4
annzrva <133603057+annzrva@users.noreply.github.com>
2025-04-08 18:45:31 +0300

    Update event data, add images

--- src/data/events.ts
+++ src/data/events.ts
@@ -2,20 +2,21 @@ import { Event, EventSpeaker } from "@/types/events";
 
 export const events: Event[] = [
   {
-    id: "event-1",
+    id: "hackathon",
+    topic: "The Caltech Longevity Hackathon",
+    date: "2025-05-24",
+    time: "10:00 AM - May 25, 7:00 PM PDT",
+    location: "Dabney Hall, Pasadena, CA",
+    featured: true,
     speakers: [{
-      name: "Ryan Cabeen",
-      title: "CTO Blueprint Bryan Johnson",
+      name: "Caltech Longevity Club",
+      title: "Hackathon",
       social: {
-        linkedin: "https://www.linkedin.com/in/cabeen/",
+        twitter: "https://x.com/caltechlongevity",
       },
-      photo: "/events/Ryan Cabeen.png",
+      photo: "/events/logo-caltech.webp"
     }],
-    topic: "Grand Opening Event: AI, Biotech & Longevity",
-    date: "2025-02-27",
-    time: "4:30 PM",
-    location: "Dabney Hall Lounge",
-    url: "https://lu.ma/ebvvob6v"
+    url: "https://lu.ma/6ndeo6qb"
   },
   {
     id: "event-2", 
@@ -28,83 +29,43 @@ export const events: Event[] = [
       photo: "/events/Matt Scholz.jpg",
     }],
     topic: "B cell therapy",
-    date: "2025-03-08",
+    date: "2025-05-08",
     time: "5:00 PM",
     location: "Dabney Hall Lounge",
+    url: "https://lu.ma/mattscholz"
   },
   {
-    id: "event-3",
+    id: "functional-training",
+    speakers: [{
+      name: "Richie Diaz",
+      title: "Professional Athlete & Trainer",
+      photo: "/events/richard diaz.jpg",
+      social: {}
+    }],
+    topic: "Functional Training Taster – How To Train like a World Athlete",
+    date: "2025-04-17",
+    time: "4:00 PM",
+    location: "Beckman Lawn",
+  },
+  {
+    id: "club-fair",
+    topic: "Caltech Club Fair",
+    date: "2025-04-13",
+    time: "TBA",
+    location: "Moore's Walk",
+    speakers: [],
+    photo: "/events/career fair.jpg"
+  },
+  // Past events
+  {
+    id: "case-study-1",
     speakers: [],
     topic: "Case Study",
     date: "2025-03-13",
     time: "4:30 PM",
     location: "Dabney Hall Lounge",
-  },
-  // {
-  //   id: "event-4",
-  //   speakers: [{
-  //     name: "Anastasia Chemeritskaya",
-  //     title: "Longevity Concierge Clinician, OB-GYN, Founder & CEO",
-  //     social: {
-  //       linkedin: "https://www.linkedin.com/in/anastasia-chemeritskaya-md-mha/",
-  //     },
-  //     photo: "/events/Anastasia Chemeritskaya.jpeg",
-  //   }],
-  //   topic: "Female Reproductive Longevity",
-  //   date: null,
-  //   time: "To be announced",
-  //   location: "To be announced",
-  // },
-  // {
-  //   id: "event-5",
-  //   speakers: [{
-  //     name: "Sebastian A. Brunemeier",
-  //     title: "Co-founder & General Partner Healthspan Capital, CEO & Founder ImmuneAGE Bio",
-  //     social: {
-  //       linkedin: "https://www.linkedin.com/in/sebastianlongbio/",
-  //       twitter: "https://x.com/sebastian_gero"
-  //     },
-  //     photo: "/events/sebastian.jpeg",
-  //   }],
-  //   topic: "Immunity Aging",
-  //   date: null,
-  //   time: "To be announced",
-  //   location: "To be announced",
-  // },
-  {
-    id: "event-6",
-    topic: "The Caltech Longevity Hackathon",
-    date: "2025-05-24",
-    time: "10:00 AM - May 25, 7:00 PM PDT",
-    location: "Dabney Hall, Pasadena, CA",
-    speakers: [{
-      name: "Caltech Longevity Club",
-      title: "Hackathon",
-      social: {
-        twitter: "https://x.com/caltechlongevity",
-      },
-      photo: "/events/logo-caltech.webp"
-    }],
-    url: "https://lu.ma/6ndeo6qb"
-  },
-
-  {
-    id: "event-7",
-    topic: "Live Case Study: Longevity Meets Performance",
-    date: "2025-04-03",
-    time: "4:30 PM – 5:30 PM",
-    location: "Dabney Hall Lounge",
-    speakers: [],
-  },
-  {
-    id: "event-8",
-    topic: "Caltech Club Fair",
-    date: "2025-04-13",
-    time: "TBA",
-    location: "TBA",
-    speakers: []
+    isPast: true
   }
-  
 ];
 
 export function getEvents() {
@@ -112,9 +73,10 @@ export function getEvents() {
   now.setHours(0, 0, 0, 0);
 
   return {
+    featured: events.filter(event => event.featured),
     upcoming: events
       .filter(event => {
-        if (!event.date) return true; // Events with no date will be shown as upcoming
+        if (!event.date || event.isPast) return false;
         const eventDate = new Date(event.date);
         eventDate.setHours(0, 0, 0, 0);
         return eventDate >= now;
@@ -126,10 +88,10 @@ export function getEvents() {
       }),
     past: events
       .filter(event => {
-        if (!event.date) return false; // Events with no date will not show as past
+        if (!event.date) return false;
         const eventDate = new Date(event.date);
         eventDate.setHours(0, 0, 0, 0);
-        return eventDate < now;
+        return eventDate < now || event.isPast;
       })
       .sort((a, b) => {
         return new Date(b.date!).getTime() - new Date(a.date!).getTime();
