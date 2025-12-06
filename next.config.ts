a9a3404d86247a94283067b1c86042fe131fc4ea
SecurityQQ <alexmsecurity@gmail.com>
2025-12-06 10:54:08 -0800

    Remove distDir configuration from Next.js config file

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
-  distDir: 'out',
   images: {
     unoptimized: true,
     remotePatterns: [
